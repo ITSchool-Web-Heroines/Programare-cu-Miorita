933ca525028580bbc8177d171f8394d8841624d8
Belei Bianca <biebianca99@gmail.com>
2022-07-28 22:06:52 +0300

    Rezolvari

--- playground-2.js
+++ playground-2.js
@@ -1,2 +1,17 @@
 // https://miorita.scoalaweb.ro/playgrounds/hatchet
 // Scrie un program care să o ajute pe Miorița să ridice baltagul și să îl aducă Vitoriei
+
+move();
+pick("hatchet");
+turnRight();
+for (let i = 0; i < 4; i++) {
+  move();
+}
+turnRight();
+turnRight();
+turnRight();
+for (let i = 0; i < 2; i++) {
+  move();
+}
+
+drop("hatchet");
--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,13 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
+
+let j = 0;
+while (j != 1) {
+  if (canMove()) {
+    j = 1;
+    move();
+  } else {
+    turnRight();
+  }
+}
