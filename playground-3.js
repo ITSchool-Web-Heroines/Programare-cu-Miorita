b4689f48114018cc7235f6162e418b9721243245
AliceIoana <aliceioana1007@gmail.com>
2022-07-29 11:36:19 +0300

    miorita challenge

--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,9 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
+reset();
+turnLeft();
+move();
+move();
+turnRight();
+//asta era toata cerinta?
