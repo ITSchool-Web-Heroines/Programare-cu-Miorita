ec513e16c6bd0aa752dc794bce854db3bf92c4a8
Semida <semidag@outlook.com>
2022-08-01 12:22:49 +0300

    another change

--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,22 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
-// Programul trebuie să funcționeze indiferent de configurația stânei.
+move();
+turnLeft();
+function superMove(times = 1)
+{
+    for(var i = 0; i < times; i = i+1) 
+    {
+        move();
+    }
+}
+superMove(3);
+turnLeft();
+superMove(2);
+turnLeft();
+superMove(6);
+turnLeft();
+superMove(2);
+turnLeft();
+superMove(3);
+turnLeft();
+superMove();
