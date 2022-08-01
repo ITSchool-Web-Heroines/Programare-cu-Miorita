01a8040df7aedef26304853a8ca20b0e6e567c1a
Semida <semidag@outlook.com>
2022-08-01 12:29:11 +0300

    change-1

--- playground-3.js
+++ playground-3.js
@@ -1,7 +1,11 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
-move();
-turnLeft();
+function turnAround() 
+{
+    turnRight()
+    turnRight()
+        turnRight()
+}
 function superMove(times = 1)
 {
     for(var i = 0; i < times; i = i+1) 
@@ -9,14 +13,17 @@ function superMove(times = 1)
         move();
     }
 }
+turnAround() 
 superMove(3);
 turnLeft();
-superMove(2);
+superMove();
 turnLeft();
 superMove(6);
 turnLeft();
 superMove(2);
 turnLeft();
-superMove(3);
+superMove(6);
 turnLeft();
-superMove();
+superMove(1);
+turnLeft();
+superMove(3);
