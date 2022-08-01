13498cba7602aeb7d94fdf682832aa52f2798390
anamariap21 <anapacurar321@gmail.com>
2022-08-01 23:13:12 +0300

    Miorita

--- playground-2.js
+++ playground-2.js
@@ -1,2 +1,45 @@
 // https://miorita.scoalaweb.ro/playgrounds/hatchet
 // Scrie un program care să o ajute pe Miorița să ridice baltagul și să îl aducă Vitoriei
+function main(){ 
+    reset()
+    goToHatchet()
+    pick("hatchet")
+    goToVictoria()
+    drop("hatchet")
+}
+
+function goToHatchet () {
+    turnAround()
+    superMove(5)
+    turnRight()
+    move()
+    turnRight()
+    superMove(4)
+
+}
+
+function goToVictoria () {
+    turnAround()
+    superMove(4)
+    turnLeft()
+    move()
+    turnLeft()
+    superMove(6)
+}
+
+function turnAround() {
+turnRight()
+turnRight()
+}
+
+function superMove(times = 1 ){
+if (times <= 0) {
+    return
+    }
+
+move()
+superMove(times - 1)
+}
+
+
+
