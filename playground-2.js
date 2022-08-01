cb02bcfb8056943345fd724139d695cb74a046d7
Mihaela Halip <105174803+hmihaella@users.noreply.github.com>
2022-08-01 21:23:35 +0300

    Update playground-2.js

--- playground-2.js
+++ playground-2.js
@@ -1,13 +1,6 @@
 /*
 reset();
 
-function turnAround()
-{
-    turnRight();
-    turnRight();
-}
-
-
 function superMove (times = 1)
 {
     for (var i = 0; i < times; i = i+1){
@@ -27,14 +20,6 @@ drop("hatchet");
 */
 
 reset();
-
-
-function turnAround()
-{
-    turnRight();
-    turnRight();
-}
-
 
 function superMove (times = 1)
 {
