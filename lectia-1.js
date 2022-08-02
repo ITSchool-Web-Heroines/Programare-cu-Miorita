039d64d03f5016c74e38d0ce568ed21048d7c202
Cristina Racolta <cristinaracolta2000@gmail.com>
2022-08-02 19:01:23 +0300

    Am modificat lectia 1

--- lectia-1.js
+++ lectia-1.js
@@ -1,3 +1,26 @@
 // https://miorita.scoalaweb.ro/lessons/1-intro
 // Ajut-o pe Miorița să ocolească gardul și să ajungă la pășune, 
 // folosind oricare din comenzile invățate la curs
+
+
+reset();
+turnRight();
+move();
+turnRight();
+turnRight();
+turnRight();
+move();
+move();
+turnRight();
+turnRight();
+turnRight();
+move();
+turnRight();
+turnRight();
+turnRight();
+move();
+Set your username:
+git config --global user.name "FIRST_NAME LAST_NAME"
+
+Set your email address:
+git config --global user.email "MY_NAME@example.com"
