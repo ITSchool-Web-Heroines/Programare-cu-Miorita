5401a9713a01a274726781b7d8958ba91c6d55ca
Andreea2194 <36506524+Andreea2194@users.noreply.github.com>
2022-08-02 11:33:54 +0300

    Update lectia-3.js

--- lectia-3.js
+++ lectia-3.js
@@ -1,2 +1,10 @@
 // https://miorita.scoalaweb.ro/lessons/3-while
 // Scrie un program care să o ajute pe Miorița să ajungă la pășunea din centrul spiralei
+reset();
+let lista = [6, 6, 6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1]
+for (let culoar = 0; culoar < lista.length; culoar++) {
+    for (let i = 0; i < lista[culoar]; i++) {
+        move();
+    }
+    turnRight();
+}
