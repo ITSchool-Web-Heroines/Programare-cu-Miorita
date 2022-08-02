f576caeff3e2ad84c3dd00e6be1f23ca4d40eb1a
Cristina Racolta <cristinaracolta2000@gmail.com>
2022-08-02 19:14:12 +0300

    The hatchet

--- playground-2.js
+++ playground-2.js
@@ -1,2 +1,25 @@
 // https://miorita.scoalaweb.ro/playgrounds/hatchet
 // Scrie un program care să o ajute pe Miorița să ridice baltagul și să îl aducă Vitoriei
+/*
+    Based on a famous Romanian novel, The hatchet (Baltagul),
+    Miorița has to find the hatchet and bring it to Vitoria.
+
+    The hatchet always starts right in front of Miorița.
+    Vitoria is always at the bottom-right corner of the sheepfold.
+*/
+reset();
+move();
+pick("hatchet")
+turnRight();
+function superMove(times = 1)
+{
+   
+    for(var i = 0; i < times; i = i+1) {
+        move()
+    }
+}
+
+superMove(4);
+turnLeft();
+superMove(3);
+drop("hatchet");
