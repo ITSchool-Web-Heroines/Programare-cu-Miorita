4edef4034a544725236e6acad85a0a7b7e74aca8
ceraselaalb <107505217+ceraselaalb@users.noreply.github.com>
2022-08-03 13:20:19 +0300

    almost done
    without playground 3

--- playground-3.js
+++ playground-3.js
@@ -1,3 +1,23 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
+reset();
+
+function superMove(times = 1){
+    if (times <= 0){
+        return
+    }
+    move()
+    superMove(times - 1)
+}
+
+if (canMove()){
+    move
+}
+
+else {
+    turnRight();
+    move();
+}
+
+superMove(5);
