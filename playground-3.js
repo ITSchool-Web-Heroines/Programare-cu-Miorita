38460d11778601a5fd51c4bf7e79df195bbd2533
Alexandra Toma <tomaalexandra8@gmail.com>
2022-08-04 15:34:56 +0300

    temar

--- playground-3.js
+++ playground-3.js
@@ -1,23 +1,18 @@
 // https://miorita.scoalaweb.ro/playgrounds/escape
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
-function turnAround(){
-    turnRight();
-}
-
-while(! canMove()){
-    turnAround();
-}
-
 function superMove(){
     let n=[];
     for(let i=n.length;i>=0;i++){
         move();
         if(!canMove()){
-            turnAround();
+           turnRight();
         }
         
     }
+}
+while(!canMove()){
+    turnRight();
 }
 if(canMove()){
     superMove();
