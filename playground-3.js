92c896ba68cbc7771f9e0fc9e4dda2ec43e264ef
NicoA8 <nicoastancai@gmail.com>
2022-08-04 18:46:18 +0300

    Varianta finala

--- playground-3.js
+++ playground-3.js
@@ -1,24 +1,6 @@
-function facefreeFence() {
-  while (canMove()) {
-    move();
-  }
-}
-function rightfreeFence() {
+while (!canMove()) {
   turnRight();
-  facefreeFence();
 }
-function tailfreeFence() {
-  turnRight();
-  facefreeFence();
-}
-function leftfreeFence() {
-  turnRight();
-  facefreeFence();
-}
-if (canMove()) {
-  facefreeFence();
-} else {
-  rightfreeFence();
-  tailfreeFence();
-  leftfreeFence();
+while (canMove()) {
+  move();
 }
