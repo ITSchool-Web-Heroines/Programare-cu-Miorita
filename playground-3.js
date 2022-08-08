f56a2a5c69d23c91a13f2fca2974cf4015656140
AliceIoana <aliceioana1007@gmail.com>
2022-08-08 10:37:58 +0300

    playground3 done

--- playground-3.js
+++ playground-3.js
@@ -2,16 +2,9 @@
 // Scrie un program care să o ajute pe Miorița să ajungă la pășune
 // Programul trebuie să funcționeze indiferent de configurația stânei.
 reset();
-// let free = canMove();
-if (canMove()) {
-  superMove();
-} else {
+while (!canMove()) {
   turnRight();
-  superMove();
 }
-
-function superMove(times = 1) {
-  for (var i = 0; i < times; i = i + 1) {
-    move();
-  }
+while (canMove()) {
+  move();
 }
